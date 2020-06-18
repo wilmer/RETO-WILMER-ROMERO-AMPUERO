7f36c1228ac470221f6250fcd2b76a545826d04a
Wilmer <wilmer_11_90@hotmail.com>
2020-06-18 03:10:40 -0500

    first commit

---
+++ API/WebApiSegura/Controllers/TipoComisarioController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApiSegura.DBModel;
+
+namespace WebApiSegura.Controllers
+{
+    [Authorize]
+    [RoutePrefix("api/tipocomisario")]
+    public class TipoComisarioController : ApiController
+    {
+        [HttpGet]
+        [Route("GetId")]
+        public IHttpActionResult GetId(int id)
+        {
+            //var customerFake = "customer-fake: " + id;
+            //return Ok(customerFake);
+            TTTipoComisario TipoComisario = new TTTipoComisario();
+            try
+            {
+                using (gestolimpEntities db = new gestolimpEntities())
+                {
+                    TipoComisario = db.TTTipoComisario.First(c => c.IdTipoComisario == id);
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            return Json(TipoComisario);
+        }
+
+        [HttpGet]
+        [Route("GetAll")]
+        public IHttpActionResult GetAll()
+        {
+            List<TTTipoComisario> listaTipoComisario = new List<TTTipoComisario>();
+            try
+            {
+                using (gestolimpEntities db = new gestolimpEntities())
+                {
+                    listaTipoComisario = db.TTTipoComisario.ToList();
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            return Json(listaTipoComisario);
+        }
+
+        [HttpPost]
+        [Route("CreateTipoComisario")]
+        public IHttpActionResult CreateTipoComisario(string descripcion)
+        {
+            TTTipoComisario model = new TTTipoComisario();
+            try
+            {
+                using (var context = new gestolimpEntities())
+                {
+
+                    model.DescriTipoComisario = descripcion;
+                    context.TTTipoComisario.Add(model);
+                    context.SaveChanges();
+
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            return Json(model);
+        }
+        [HttpPost]
+        [Route("UpdateTipoComisario")]
+        public IHttpActionResult UpdateTipoComisario(int id, string descripcion)
+        {
+            TTTipoComisario model = new TTTipoComisario();
+            try
+            {
+                using (var context = new gestolimpEntities())
+                {
+                    model.IdTipoComisario = id;
+                    model.DescriTipoComisario = descripcion;
+                    context.Entry(model).State = System.Data.Entity.EntityState.Modified;
+                    context.SaveChanges();
+
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            return Json(model);
+        }
+
+        [HttpPost]
+        [Route("DeleteTipoComisario")]
+        public IHttpActionResult DeleteTipoComplejo(int id)
+        {
+            TTTipoComisario model = new TTTipoComisario();
+            try
+            {
+                using (var context = new gestolimpEntities())
+                {
+                    model = context.TTTipoComisario.Find(id);
+                    context.TTTipoComisario.Remove(model);
+                    context.SaveChanges();
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            return Json(model);
+        }
+    }
+}
---
+++ API/WebApiSegura/Controllers/TipoComplejoController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApiSegura.DBModel;
+
+namespace WebApiSegura.Controllers
+{
+    [Authorize]
+    [RoutePrefix("api/tipocomplejo")]
+    public class TipoComplejoController : ApiController
+    {
+  
+        [HttpGet]
+        [Route("GetId")]
+        public IHttpActionResult GetId(int id)
+        {
+            //var customerFake = "customer-fake: " + id;
+            //return Ok(customerFake);
+            TTTipoComplejo TipoComplejo = new TTTipoComplejo();
+            try
+            {
+                using (gestolimpEntities db = new gestolimpEntities())
+                {
+                    TipoComplejo = db.TTTipoComplejo.First(c=>c.IdTipoComplejo==id);
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            return Json(TipoComplejo);
+        }
+
+        [HttpGet]
+        [Route("GetAll")]
+        public IHttpActionResult GetAll()
+        {
+            List<TTTipoComplejo> listaTipoComplejo = new List<TTTipoComplejo>();
+            try
+            {
+                using (gestolimpEntities db = new gestolimpEntities())
+                {
+                     listaTipoComplejo = db.TTTipoComplejo.ToList();
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            return Json(listaTipoComplejo);
+        }
+
+        [HttpPost]
+        [Route("CreateTipoComplejo")]
+        public IHttpActionResult CreateTipoComplejo(string descripcion)
+        {
+            TTTipoComplejo model = new TTTipoComplejo();
+            try
+            {
+                using (var context = new gestolimpEntities())
+                {
+                   
+                    model.DescriTipoComplejo = descripcion;
+                    context.TTTipoComplejo.Add(model);
+                    context.SaveChanges();
+
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            return Json(model);
+        }
+        [HttpPost]
+        [Route("UpdateTipoComplejo")]
+        public IHttpActionResult UpdateTipoComplejo(int id,string descripcion)
+        {
+            TTTipoComplejo model = new TTTipoComplejo();
+            try
+            {
+                using (var context = new gestolimpEntities())
+                {
+                    model.IdTipoComplejo = id;
+                    model.DescriTipoComplejo = descripcion;
+                    context.Entry(model).State= System.Data.Entity.EntityState.Modified;
+                    context.SaveChanges();
+
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            return Json(model);
+        }
+
+        [HttpPost]
+        [Route("DeleteTipoComplejo")]
+        public IHttpActionResult DeleteTipoComplejo(int id)
+        {
+           TTTipoComplejo model = new TTTipoComplejo();
+            try
+            {
+                using (var context = new gestolimpEntities())
+                {
+                     model = context.TTTipoComplejo.Find(id);
+                    context.TTTipoComplejo.Remove(model);
+                    context.SaveChanges();
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            return Json(model);
+        }
+    }
+}
